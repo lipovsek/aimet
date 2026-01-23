Fix bug in numerical stability check
Signed-off-by: Kyunggeun Lee <quic_kyunggeu@quicinc.com>
Co-authored-by: Kyunggeun Lee <quic_kyunggeu@quicinc.com>

TrainingExtensions/torch/src/python/aimet_torch/v2/quantization/affine/backends/torch_builtins.py
@@ -66,8 +66,9 @@ def _is_numerically_stable(dtype: torch.dtype, qmin: int, qmax: int):
         return True
 
     # NOTE: This is a heuristic criteria. It doesn't perfectly guarantee numerical stability
-    #       This criteria allows 8-bit quantization of float16, but it needs more discussion
-    if torch.finfo(dtype).eps > 1e-1 / (qmax - qmin):
+    #       This criteria allows up to 8-bit quantization with float16
+    #       and 4-bit quantization with bfloat16
+    if torch.finfo(dtype).eps > 0.25 / (qmax - qmin):
         return False
 
     return True

TrainingExtensions/torch/test/python/v2/quantization/affine/backends/test_backend.py
@@ -958,7 +958,7 @@ class TestQuantizationBackends:
 
 
 @pytest.mark.parametrize(
-    "qmin,   qmax,    offset",
+    "qmin, qmax, offset",
     [
         (-8, 7, 0),
         (0, 15, 0),
@@ -1025,30 +1025,15 @@ def test_cross_validate_torch_fake_quantize(
         tensor, scale.detach(), offset.detach(), qmin, qmax
     )
 
-    assert torch.allclose(out1, expected, atol=atol)
-    assert torch.allclose(out2, expected, atol=atol)
+    assert torch.allclose(out1, expected, atol=atol, rtol=1e-3)
+    assert torch.allclose(out2, expected, atol=atol, rtol=1e-3)
     if out3 is not None:
-        assert torch.allclose(out3, expected, atol=atol)
+        assert torch.allclose(out3, expected, atol=atol, rtol=1e-3)
 
-    scale = torch.stack(
-        [
-            scale,
-            scale,
-        ]
-    )
-    offset = torch.stack(
-        [
-            offset,
-            offset,
-        ]
-    )
+    scale = torch.stack([scale, scale])
+    offset = torch.stack([offset, offset])
     tensor = torch.stack([tensor, tensor])
-    expected = torch.stack(
-        [
-            expected,
-            expected,
-        ]
-    )
+    expected = torch.stack([expected, expected])
 
     out1 = torch_builtins.quantize_dequantize(tensor, scale, offset, qmin, qmax)
     out2 = torch_builtins.QuantDequantFunc.apply(tensor, scale, offset, qmin, qmax).to(
@@ -1058,7 +1043,7 @@ def test_cross_validate_torch_fake_quantize(
         tensor, scale.detach(), offset.detach(), qmin, qmax
     )
 
-    assert torch.allclose(out1, expected, atol=atol)
-    assert torch.allclose(out2, expected, atol=atol)
+    assert torch.allclose(out1, expected, atol=atol, rtol=1e-3)
+    assert torch.allclose(out2, expected, atol=atol, rtol=1e-3)
     if out3 is not None:
-        assert torch.allclose(out3, expected, atol=atol)
+        assert torch.allclose(out3, expected, atol=atol, rtol=1e-3)