57a7789d138dc155f92cf361648f7166a90383ef
Kyunggeun Lee <kyunggeu@qti.qualcomm.com>
2026-08-18 13:48:08 -0700

    Update xfail message for _safe_softmax aten-qnn ir alignment test
    Signed-off-by: Kyunggeun Lee <kyunggeu@qti.qualcomm.com>

--- TrainingExtensions/torch/test/python/v2/experimental/test_ir_alignment.py
+++ TrainingExtensions/torch/test/python/v2/experimental/test_ir_alignment.py
@@ -407,7 +407,7 @@ supergroups = {
 xfail = {
     torch.ops.aten._native_batch_norm_legit: "ONNX export error",
     torch.ops.aten._native_batch_norm_legit_no_training: "ONNX export error",
-    torch.ops.aten._safe_softmax: "Where's inputs is missing encoding",
+    torch.ops.aten._safe_softmax: "Onnxscript bug. See https://github.com/microsoft/onnxscript/issues/2993",
     torch.ops.aten.clamp: "Clip min/max are missing encoding",
     torch.ops.aten.clip: "Clip min/max are missing encoding",
     torch.ops.aten.copy: "ONNX export error",
